395290b35d3380eab1571d9057e734227875fa89
Isaac Gardner <Isaacjamesgardner@hotmail.com>
2024-04-09 16:11:45 +0100

    added input validation

--- task_e.c
+++ task_e.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char *argv[]){
     
+	
+	if (argc != 2){
+        printf("Error. Expected 1 filename.");
+        return 1;
+    }
+
+    int length = strlen(argv[1]);
+    char filename[] = argv[1];
+
+    if (filename[length-1] != 't' || filename[length-2] != 'x' || filename[length-3] != 't'|| filename[length-4] != '.'){
+        printf("Error. Cannot open requested file.");
+        return 1;
+    }
 	
 	
 	// Read number from file - do not edit
