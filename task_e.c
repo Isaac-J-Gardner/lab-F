b49cf5dc9c80b6f97acb623a42b81b1936e640df
Isaac Gardner <Isaacjamesgardner@hotmail.com>
2024-04-09 16:44:28 +0100

    this is rediculous

--- task_e.c
+++ task_e.c
@@ -6,6 +6,14 @@ int main(int argc, char *argv[]){
         printf("Error. Expected 1 filename.");
         return 1;
     }
+
+    FILE *fptr;
+	fptr = fopen(argv[1], "r");
+
+    if (fptr == NULL){
+        printf("Error. Cannot open requested file.");
+        return 1;
+    }
 	
 	
 	// Read number from file - do not edit
@@ -17,10 +25,6 @@ int main(int argc, char *argv[]){
 	
 
 	// Enter your code from Lab D to print JSON output (with name, email, number1, number2) goes under here
-    if (fptr == NULL){
-        printf("Error. Cannot open requested file.");
-        return 1;
-    }
     printf("{\"name\": \"Isaac Gardner\",\n \"email\": \"isaac.gardner-2@student.manchester.ac.uk\",\n \"number1\": %.2f,\n \"number2\": %.3e}", num, num);
 	
 
